d5ba9915e7aaaddb184d9313301fbb82eb08d4f3
Benjamin J. Balter <ben@balter.com>
2012-01-12 16:15:48 -0500

    added readme, removed php

--- api.php
+++
@@ -1,7 +0,0 @@
-<?php
-$base = 'http://www.broadbandmap.gov';
-$url = $base . $_GET['url'];
-unset( $_GET['url'] );
-$url .= '?' . http_build_query( $_GET, '', '&' );
-echo file_get_contents( $url );
-?>
